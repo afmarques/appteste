93ec324a1c588f72ee5bc6e4e2b9263341876007
andre marques <andre.f.marques@gmail.com>
2013-08-26 20:02:29 +0100

    Baseline

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Appteste::Application.config.secret_token = '1f54a25848ab801419a8cc3b42e1acf091b96e17a7cbd60d597743f47d2e04894755130176af7b92418ef5f1fc5aecc933285bebe837a99b4ee5e56797d304cc'
